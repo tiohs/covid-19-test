a5807073925d4af7bd2b90b166256701b0b26da8
Hamilton Silva <hamiltonsilva.dev@gmail.com>
2024-02-06 20:42:22 +0100

    updated

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -1,14 +1,16 @@
 <template>
-  <h1 class="text-xl font-bold">Confirma o numero de casos de covid em cada pais </h1>
+  
   <div class="flex justify-center items-center min-h-screen bg-gray-100">
-    <div class="w-full max-w-xl p-4 bg-white rounded shadow">
-      <canvas ref="barChartCanvas" class="w-full h-100"></canvas>
-      <div class="flex justify-between mt-4">
-        <button @click="changePage(-1)" class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Anterior</button>
-        <button @click="changePage(1)" class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Próximo</button>
-      </div>
+  <div class="w-4/5 max-w-xl p-4 bg-white rounded shadow mx-auto">
+    <h1 class="text-center text-xl font-bold text-blue-500">Confirma o número de casos de covid em cada país</h1>
+    <canvas ref="barChartCanvas" class="w-full h-100"></canvas>
+    <div class="flex justify-between mt-4">
+      <button @click="changePage(-1)" class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Anterior</button>
+      <button @click="changePage(1)" class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Próximo</button>
     </div>
   </div>
+</div>
+
 </template>
 
 <script>
